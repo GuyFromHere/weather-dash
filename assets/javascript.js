9f0f85f7777dda2158e2275e601872fc2ee70faf
Dan Dismuke <dandismuke@gmail.com>
2019-11-20 19:28:50 -0800

    Basic api calls up and running with simple page structure in place.

--- assets/javascript.js
+++ assets/javascript.js
@@ -12,7 +12,10 @@ $(document).ready(function() {
   var searchBar = $("<div>").addClass("searchBar");
   var searchTitle = $("<h3>").text("Search for a City:");
   var searchInput = $("<input>").addClass("searchInput");
-  var cityList = $("<ul>").addClass("cityList");
+  var searchIcon = $("<i>")
+    .addClass("fas fa-search")
+    .attr("id", "citySearch");
+  var listCity = $("<ul>").addClass("listCity");
   // Selected City
   var selectedCity = $("<div>").addClass("selectedCity");
   // Forecast
@@ -23,15 +26,73 @@ $(document).ready(function() {
 
   // Append elements
   $(".header").append(title);
-
   $(".container").append(nav);
   nav.append(searchBar);
   searchBar.append(searchTitle);
   searchBar.append(searchInput);
-  searchBar.append(cityList);
-
+  searchInput.after(searchIcon);
+  searchBar.append(listCity);
   $(".container").append(selectedCity);
-
   $(".container").append(forecast);
   forecast.append(forecastTitle);
+
+  // Add Content
+
+  /************************ TEST */
+  // Build API Call
+  const apikey = "f2919aeb80a924ffadb75fdf60e7f195";
+  var request =
+    "http://api.openweathermap.org/data/2.5/forecast?q=portland&APPID=" +
+    apikey;
+
+  //var req = "http://api.openweathermap.org/data/2.5/forecast?q=";
+
+  // City List Event Handler
+  var listCityNav = $("<li>").addClass("listCityNav");
+  listCity.append(listCityNav);
+  listCityNav.text("Portland");
+
+  $(".listCityNav").on("click", function() {
+    getCity($(this).text());
+  });
+  //});
+
+  $("#citySearch").on("click", function() {
+    var newCity = searchInput.val();
+
+    getCity(newCity);
+  });
+
+  function getCity(city) {
+    var req =
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&APPID=" +
+      apikey;
+    console.log(req);
+
+    var forecastCityName = $("<h2>");
+    var listForecast = $("<ul>");
+    var listTemp = $("<li>");
+    var listHumidity = $("<li>");
+    var listWindSpd = $("<li>");
+    var listUV = $("<li>");
+    $.ajax({
+      url: req,
+      method: "GET"
+    }).then(function(res) {
+      forecastCityName.text(res.city.name);
+      listTemp.text("Temperature: " + res.list[0].main.temp);
+      listHumidity.text("Humidity: " + res.list[0].main.temp);
+      listWindSpd.text("Wind Speed: " + res.list[0].wind.speed);
+
+      selectedCity.append(forecastCityName);
+      selectedCity.append(listForecast);
+      listForecast.append(listTemp);
+      listForecast.append(listHumidity);
+      listForecast.append(listWindSpd);
+
+      console.log(res);
+    });
+  }
 });
